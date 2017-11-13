334486ca1bffbc735c66912ca23685d342ac16ad
Pothanaboyina S Ram <Pothanaboyina S Ram@DESKTOP-ATQV3B6>
2017-11-14 02:13:43 +0530

    just adding decorate for check

--- BikeSelect/src/com/app/Main.java
+++ BikeSelect/src/com/app/Main.java
@@ -9,4 +9,7 @@ public class Main {
 		System.out.println("====================================");
 	}
 
+	public void decorate(){
+		System.out.println("****++++====%%%%----@@@@^^^^$$$$!!!!####");
+	}
 }
